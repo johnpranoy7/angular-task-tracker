439d097f47e82d09e480c44884ec6544d3e00f30
johnpranoy7 <47387210+johnpranoy7@users.noreply.github.com>
2022-11-14 20:00:23 +0530

    Initial Changes

---
+++ src/app/dummyTasks.ts
@@ -0,0 +1,22 @@
+import {Task} from './Task';
+
+export const Tasks:Task[] =[
+    {
+        id: 1,
+        name: 'Eating',
+        day: '15th Nov 2022',
+        reminder: false
+    },
+    {
+        id: 2,
+        name: 'Swimming',
+        day: '17th Nov 2022',
+        reminder: false
+    },
+    {
+        id: 4,
+        name: 'Sleeping',
+        day: '18th Nov 2022',
+        reminder: true
+    }
+]
